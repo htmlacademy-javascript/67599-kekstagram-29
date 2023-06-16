a5a68694821f036590a6dbaa87d8e4879f9db9e7
pavalex <Alex.P79@mail.ru>
2023-06-16 10:54:49 +0300

    Исправил замечания

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,15 @@
 // Функция для проверки длины строки
 const checkLength = (str, maxLength) => str.length <= maxLength;
 
-console.log(checkLength('Программирование', 20));
-console.log(checkLength('Программирование', 10));
+checkLength('Программирование', 20);
 
 // Функция для проверки, является ли строка палиндромом
 const checkPalindrome = (str) => {
-  str = str.toLowerCase().replaceAll(' ', '');
+  if (typeof str !== 'string') {
+    str = str.toString();
+  }
+
+  str = str.replaceAll(' ', '').toLowerCase();
   let newStr = '';
 
   for (let i = str.length - 1; i >= 0; i--) {
@@ -16,16 +19,17 @@ const checkPalindrome = (str) => {
   return newStr === str;
 };
 
-console.log(checkPalindrome('Лёша на полке клопа нашёл '));
-console.log(checkPalindrome('Кекс'));
+checkPalindrome('Лёша на полке клопа нашёл ');
 
 // Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа
 const getPositiveInteger = (str) => {
-  str = str.toString();
+  if (typeof str !== 'string') {
+    str = str.toString();
+  }
 
   let num = '';
 
-  for (let i = 0; i <= str.length - 1; i++) {
+  for (let i = 0; i < str.length; i++) {
     if (!Number.isNaN(parseInt(str[i], 10))) {
       num += str[i];
     }
@@ -34,6 +38,4 @@ const getPositiveInteger = (str) => {
   return parseInt(num, 10);
 };
 
-console.log(getPositiveInteger('1 кефир, 0.5 батона'));
-console.log(getPositiveInteger('а я томат'));
-console.log(getPositiveInteger(-1));
+getPositiveInteger('1 кефир, 0.5 батона');
